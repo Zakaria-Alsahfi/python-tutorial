5ac22aab8159f34679ef5379b850ecb0f094a2f0
sa7fiz <zakaria.sahfi@gmail.com>
2019-02-26 05:18:52 -0600

    Add files via upload

---
+++ code/Integers&Floats.py
@@ -0,0 +1,63 @@
+# Arithmetic Operates:
+# Addition:      3 + 2
+# Subtraction:   3 - 2
+# Multiplication: 3 * 2
+# Division:      3 / 2
+# Floor Division 3 // 2
+# Exponent:      3 ** 2
+# Modulus:       3 % 2
+
+# Floor Division difference than Division it print number without decimals
+
+# if we want to print a number to power we use Exponent
+# Modulus will print the remain after division also, to tell if the number is even or odd
+
+x = 3
+y = 2
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x ** y)
+print(x % y)
+
+# also Order of operations works
+# to increment a number
+
+num = 5
+num += 2
+num *= 10
+print(num)
+
+# Building functions
+# 1- Absolute value
+print(abs(-3))
+
+# 2- Round off decimal
+print(round(3.74))
+
+# 3- Round off decimal to specific decimal digits
+print(round(3.75, 1))
+
+# 4- Comparison between numbers
+# Equal:            3 == 2
+# Not Equal:        3 != 2
+# Greater Than:     3 > 2
+# Less Than:         3 < 2
+# Greater or Equal:  3 >= 2
+# Less or Equal:     3 <= 2
+
+z = 3
+j = 2
+print(z == j)
+print(z != j)
+print(z > j)
+print(z < j)
+print(z >= j)
+print(z <= j)
+
+# Cast from String to Int
+num_1 = int('100')
+num_2 = int('200')
+print(num_1 + num_2)
